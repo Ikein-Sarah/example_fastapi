d9e7c8426bb96ea1ce439e0056a1cb8f3eafc34a
elosarah85 <elosarah85@gmail.com>
2025-10-21 10:57:58 +0100

    Fix database URL handling for Heroku

--- app/database.py
+++ app/database.py
@@ -5,6 +5,8 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from .config import settings
+import os
+
 
 
 #SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
