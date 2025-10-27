cf353d31385feb15636a6ce80139440b25a3233c
elosarah85 <elosarah85@gmail.com>
2025-10-27 20:13:58 +0100

    add proper migration for Heroku

---
+++ alembic/versions/dbec9dcce6bf_create_all_tables_for_heroku.py
@@ -0,0 +1,57 @@
+"""create all tables for heroku
+
+Revision ID: dbec9dcce6bf
+Revises: 
+Create Date: 2025-10-26 23:49:01.132191
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'dbec9dcce6bf'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+     op.create_table(
+        'users',
+        sa.Column('id', sa.Integer(), primary_key=True, nullable=False),
+        sa.Column('email', sa.String(), nullable=False, unique=True),
+        sa.Column('password', sa.String(), nullable=False),
+        sa.Column('created_at', sa.TIMESTAMP(timezone=True), server_default=sa.text('now()'), nullable=False),
+        sa.Column('phone_number', sa.String()),
+    )
+
+    # Create posts table
+     op.create_table(
+        'posts',
+        sa.Column('id', sa.Integer(), primary_key=True, nullable=False),
+        sa.Column('title', sa.String(), nullable=False),
+        sa.Column('content', sa.String(), nullable=False),
+        sa.Column('published', sa.Boolean(), server_default='TRUE', nullable=False),
+        sa.Column('created_at', sa.TIMESTAMP(timezone=True), server_default=sa.text('now()'), nullable=False),
+        sa.Column('owner_id', sa.Integer(), sa.ForeignKey('users.id', ondelete='CASCADE'), nullable=False),
+    )
+
+    # Create votes table
+     op.create_table(
+        'votes',
+        sa.Column('user_id', sa.Integer(), sa.ForeignKey('users.id', ondelete='CASCADE'), primary_key=True),
+        sa.Column('post_id', sa.Integer(), sa.ForeignKey('posts.id', ondelete='CASCADE'), primary_key=True),
+    )
+
+     pass
+
+
+def downgrade() -> None:
+    op.drop_table('votes')
+    op.drop_table('posts')
+    op.drop_table('users')
+
+    pass
